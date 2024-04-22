6a8b3e40be179c3588a9abbd85c09dffaa703620
Mustafa Sahin <mustafasahin.6123@gmail.com>
2024-04-22 16:30:13 +0200

    Update line detection

--- lane_detection.py
+++ lane_detection.py
@@ -14,7 +14,7 @@ class LaneDetection:
 
         # Definiere Schwellenwerte für Grautöne
         lower_gray = np.array([0, 0, 0])
-        upper_gray = np.array([180, 255, 110])  # Helligkeit bis 110 für Grautöne
+        upper_gray = np.array([180, 255, 110])  # Helligkeit bis 100 für Grautöne
 
         # Erstelle eine Maske, die nur Grau- und Schwarztöne zulässt
         mask_gray = cv2.inRange(hsv, lower_gray, upper_gray)
@@ -25,27 +25,15 @@ class LaneDetection:
         # Erstelle ein schwarzes Bild der gleichen Größe wie das Originalbild
         black_image = np.zeros_like(observation)
 
-        # Verwende die Hough-Transformation, um Linien im Bild zu erkennen
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=50, maxLineGap=100)
-
-        if lines is not None:
-            for line in lines:
-                x1, y1, x2, y2 = line[0]
-                if x2 != x1:
-                    slope = (y2 - y1) / (x2 - x1)
-                    if slope > 0:
-                        # Zeichne rechte Linie in Rot
-                        cv2.line(black_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                    else:
-                        # Zeichne linke Linie in Blau
-                        cv2.line(black_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        # Zeichne die weißen Ränder auf das schwarze Bild
+        black_image[edges == 255] = (255, 255, 255)
 
         # Füge am unteren Rand des Bildes einen schwarzen Balken hinzu
-        balken_hoehe = 13
-        black_image[-balken_hoehe:] = 0
+        balken_hoehe = 13  # Höhe des Balkens in Pixeln
+        black_image[-balken_hoehe:] = 0  # Setze die unteren 'balken_hoehe' Pixel auf Schwarz
 
         # Blende einen Kasten in der Mitte des Bildes aus
-        mitte_x, mitte_y = (47, 71)
+        mitte_x, mitte_y = (47,71)
         black_image[mitte_y - 12 // 2:mitte_y + 12 // 2,
         mitte_x - 8 // 2:mitte_x + 8 // 2] = 0
 
