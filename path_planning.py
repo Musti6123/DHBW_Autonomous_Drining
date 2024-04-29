cb94178d712fef659e6b3814d9519bcf3699812a
Mustafa Sahin <mustafasahin.6123@gmail.com>
2024-04-29 16:59:26 +0200

    Update line detection Update test_path_planning

--- path_planning.py
+++ path_planning.py
@@ -8,5 +8,10 @@ class PathPlanning:
     def __init__(self):
         pass
 
+    def getDistance(self, point):
+
     def plan(self, left_lane_boundary: np.ndarray, right_lane_boundary: np.ndarray):
+        for i in len(left_lane_boundary):
+            for j in len(right_lane_boundary):
+
         return left_lane_boundary
