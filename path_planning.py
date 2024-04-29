00bbaf98635579beea834e009874f875993bd27c
Mustafa Sahin <mustafasahin.6123@gmail.com>
2024-04-29 18:30:52 +0200

    Update path_planning

--- path_planning.py
+++ path_planning.py
@@ -13,10 +13,17 @@ class PathPlanning:
             return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
         def midpoint(point1: tuple, point2: tuple) -> tuple:
-            return (int((point1[0] + point2[0]) / 2), int((point1[1] + point2[1]) / 2))
+            offset = 2
+            x_dif = int(point2[0] - point1[0])
+            y_dif = int(point2[1] - point1[1])
+            x_mid = int(x_dif / 2)
+            y_mid = int(y_dif / 2)
+
+
+            return (point1[0] + x_mid, point1[1] + y_mid)
 
         if len(left_lane_boundary) == 0 or len(right_lane_boundary) == 0:
-            return np.array([])  # Return empty array if any boundary is empty
+            return np.array([])
 
         path = []
         last_right_index = 0
@@ -24,7 +31,7 @@ class PathPlanning:
         for i, left_point in enumerate(left_lane_boundary):
             # Search for corresponding right point starting from last found right index
             for offset, right_point in enumerate(right_lane_boundary[last_right_index:], start=last_right_index):
-                if 17 < distance(left_point, right_point) < 27:
+                if 17 < distance(left_point, right_point) < 26:
                     path.append(midpoint(left_point, right_point))
                     last_right_index = offset  # Update last_right_index to current
                     break  # Move to the next left point after a match is found
