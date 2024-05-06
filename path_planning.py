306da8b529c12049834a26a7cbd663af75c62ff2
Mustafa Sahin <mustafasahin.6123@gmail.com>
2024-05-06 17:27:23 +0200

    Update path planning

--- path_planning.py
+++ path_planning.py
@@ -4,16 +4,15 @@ import numpy as np
 
 class PathPlanning:
     def __init__(self):
-        # Constructor can be used for initialization if needed
         pass
 
-    def plan(self, left_lane_boundary: np.ndarray, right_lane_boundary: np.ndarray) -> np.ndarray:
+    def plan(self, left_lane_boundary: np.ndarray, right_lane_boundary: np.ndarray) -> (np.ndarray, np.ndarray):
 
         def distance(point1: tuple, point2: tuple) -> float:
-            return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+            return math.sqrt((point1[0] - point2[0]) ** 2 + ((point1[1] - point2[1]) * 0.6) ** 2)
+
 
         def midpoint(point1: tuple, point2: tuple) -> tuple:
-            offset = 2
             x_dif = int(point2[0] - point1[0])
             y_dif = int(point2[1] - point1[1])
             x_mid = int(x_dif / 2)
@@ -23,17 +22,17 @@ class PathPlanning:
             return (point1[0] + x_mid, point1[1] + y_mid)
 
         if len(left_lane_boundary) == 0 or len(right_lane_boundary) == 0:
-            return np.array([])
+            return np.array([]), np.array([])
 
         path = []
         last_right_index = 0
 
         for i, left_point in enumerate(left_lane_boundary):
-            # Search for corresponding right point starting from last found right index
             for offset, right_point in enumerate(right_lane_boundary[last_right_index:], start=last_right_index):
-                if 17 < distance(left_point, right_point) < 26:
+                if 19.5 < distance(left_point, right_point) < 21:
                     path.append(midpoint(left_point, right_point))
-                    last_right_index = offset  # Update last_right_index to current
-                    break  # Move to the next left point after a match is found
+                    last_right_index = offset
+                    break
 
-        return np.array(path)  # Convert list to numpy array for consistency with input types
+        path.append((48,48))
+        return np.array(path), np.array([])
